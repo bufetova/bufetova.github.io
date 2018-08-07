2185ef40010ca3f276f3ae6a39d968e66b74fab7
Agata Budzinska <agatabudzinska@gmail.com>
2018-08-07 21:53:29 +0200

    Getting data from server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
-
 import './App.css'
 
 class App extends React.Component {
+
+  state = {
+    contacts: []
+  };
+
+componentDidMount() {
+  fetch("http://localhost:3000/contacts")
+    .then(response => response.json())
+    .then(contacts => this.setState({contacts})
+
+    )
+}
+
   render() {
+
     return (
       <div className="App">
         <h1>Contact List</h1>
@@ -12,7 +25,12 @@ class App extends React.Component {
           <button>Add contact</button>
         </form>
         <ul>
-
+          {this.state.contacts.map(contact => (
+              <li key={contact.id}>
+                {contact.firstName} {contact.lastName} {contact.phoneNumber} {contact.email}
+              </li>
+            )
+          )}
         </ul>
       </div>
     );
